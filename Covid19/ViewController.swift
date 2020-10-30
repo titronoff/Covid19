7b56a95b26468a51672d0c76b2459bb7d6c88763
Pavel Ivanovski <p@singulart.io>
2020-10-30 10:59:13 +0300

    Initial Commit

---
+++ Covid19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Covid19
+//
+//  Created by Pavel Ivanovski on 10/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
