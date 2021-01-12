db48213a84f5b15bce7fa1d93e49a14d6a94eb59
Pavel Ivanovski <p@singulart.io>
2021-01-12 20:22:55 +0300

    Validation logic

--- Covid19/Login/LoginValidator.swift
+++ Covid19/Login/LoginValidator.swift
@@ -10,10 +10,36 @@ import UIKit
 class LoginValidator {
     
     static func loginCheck(_ usernameText: String, _  passwordText: String) -> Bool {
-        if usernameText != "" && passwordText != "" {
+        if usernameText != "" && validatePassword(passwordText) {
             return true
         } else {
             return false
         }
     }
+    
+    private static func validatePassword(_ passwordText: String) -> Bool {
+        if passwordText.count >= 8 && differentCaseExisting(passwordText) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    private static func differentCaseExisting(_ str: String) -> Bool {
+        var upperCase = false
+        var lowerCase = false
+        for char in str {
+            if char.isUppercase {
+                 upperCase = true
+             }
+            if char.isLowercase {
+                lowerCase = true
+            }
+         }
+        if upperCase && lowerCase {
+            return true
+        } else {
+            return false
+        }
+     }
 }
